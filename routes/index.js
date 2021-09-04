ad9dcab2e8bcfedbf8e9ceab7ea466f8360499f3
Sukriti-sood <soodsukriti1111@gmail.com>
2021-09-04 21:50:49 +0530

    play route is ready

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const app = require("../app");
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,19 @@ router.get("/", function(req, res, next) {
     res.render("home");
 });
 
+router.get("/play/:level", (req, res) => {
+    var level = req.params.level;
+    if (level == "easy") {
+        console.log(level);
+    }
+    if (level == "medium") {
+        console.log(level);
+    }
+    if (level == "hard") {
+        console.log(level);
+    }
+    res.render("play", {
+        level: level,
+    });
+});
 module.exports = router;
